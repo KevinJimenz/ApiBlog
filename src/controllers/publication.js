c058b791529fbcb6143aac343f5b442595b578a4
Kav <kevincamilo56@gmail.com>
2024-08-09 18:48:40 -0500

    Tuve que corregir la tabla en la base de datos, vuelvo a como estaba antes

--- src/controllers/publication.js
+++ src/controllers/publication.js
@@ -13,7 +13,6 @@ export const create = async (req,res) =>{
     const category = req.params.id;
     const file = req.file.path;
     const newPublication = await publicationModel.create({
-        id: 0,
         title,
         description,
         photo: file,
