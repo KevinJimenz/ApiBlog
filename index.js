38b148e0183e473a60cb94250e9e7e43a77ff20d
Kav <kevincamilo56@gmail.com>
2024-08-30 07:08:59 -0500

    Reviso el cors options

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import {sequelize} from "./src/models/db.js" // Conexion a base de datos
 // Configuracion cors para evitar el "Cross-Origin Request Blocked" 'http://localhost:5173'
 const corsOptions = {
   origin: '*', // Especifico la direccion de origen de la peticion
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Indico que peticiones http se van usar
+  methods: ['GET', 'POST', 'UPDATE', 'DELETE'], // Indico que peticiones http se van usar
   allowedHeaders: ['Content-Type', 'Authorization'] // Autoriza a los headers
 };
 
